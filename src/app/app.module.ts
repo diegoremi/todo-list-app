570b3348ef72113b907b00474357e1174139e79f
Diego Remicio <Diego.Remicio@united.com>
2023-09-05 16:21:22 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputFormComponent } from './components/input-form/input-form.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent,
+    FilterComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
